736229f57e6f52fa48287e6cbe77625cbb36ac21
Serdar Duran <duranser@itu.edu.tr>
2025-04-06 21:20:13 +0300

    Update i2c_test_tb.v

--- test/i2c_test_tb.v
+++ test/i2c_test_tb.v
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-`default_nettype none
 
 
 module i2c_test_tb();
@@ -59,37 +58,34 @@ module i2c_test_tb();
 
     initial
     begin
-       #10000;
+       #(100*SCL_PERIOD);
        rst = 1;
-       #10000;
+       #(10*SCL_PERIOD);
        rst = 0;
-       #10000;
+       #(1000*SCL_PERIOD);
+       
        
        write_proc( 7'b001_1001, 7'b001_1101, 1'b0, 8'b0101_1001 );
-       write_proc( 7'b001_1001, 7'b001_1101, 1'b0, 8'b0101_1001 );
-       write_proc( 7'b001_1001, 7'b001_1101, 1'b0, 8'b0101_1001 );
-       
        read_proc( 7'b001_1001, 7'b001_1101, 1'b0, 8'b0101_1001 );
-       read_proc( 7'b001_1001, 7'b001_1101, 1'b0, 8'b0101_1001 );
-       read_proc( 7'b001_1001, 7'b001_1101, 1'b0, 8'b0101_1001 );
+
     end
     
 	task write_proc( input [6:0] address_slv1, input [6:0] address_slv2, input sel_slv, input [7:0] data_write );
     begin
-        data_wr = data_write;
-        write   = 1;
-        read    = 0;
-
-        addr    = sel_slv ? address_slv2 : address_slv1;
+        data_wr   = data_write;
+        addr      = sel_slv ? address_slv2 : address_slv1;
         addr_slv1 = address_slv1;
         addr_slv2 = address_slv2;
 
-        #(2*SCL_PERIOD);
+        #(SCL_PERIOD);
+        write   = 1;
+        read    = 0;
+        #(SCL_PERIOD);
         write   = 0;   
               
-        #(22*SCL_PERIOD);
+        #(21*SCL_PERIOD);
         $display("%b =? %b | %s", data_wr, sel_slv ? data_wr_slv2 : data_wr_slv1, data_wr == (sel_slv ? data_wr_slv2 : data_wr_slv1 ) ? "True" : "False");
-        #(1*SCL_PERIOD);
+        #(SCL_PERIOD);
     end
 	endtask    
 
@@ -97,19 +93,19 @@ module i2c_test_tb();
     begin
         data_rd_slv1 = data_read & ~sel_slv;
         data_rd_slv2 = data_read & sel_slv;
-        write   = 0;
-        read    = 1;
 
-        addr    = sel_slv ? address_slv2 : address_slv1;
+        addr      = sel_slv ? address_slv2 : address_slv1;
         addr_slv1 = address_slv1;
         addr_slv2 = address_slv2;
-        
-        #(2*SCL_PERIOD);
-        read    = 0;        
 
-        #(22*SCL_PERIOD);
+        #(SCL_PERIOD);
+        write   = 0;
+        read    = 1;
+        #(SCL_PERIOD);
+        read    = 0;       
+        #(21*SCL_PERIOD);
         $display("%b =? %b | %s", data_rd, sel_slv ? data_rd_slv2 : data_rd_slv1, data_rd == (sel_slv ? data_rd_slv2 : data_rd_slv1 ) ? "True" : "False");
-        #(1*SCL_PERIOD);
+        #(SCL_PERIOD);
     end
 	endtask    
     
